c99bbe8eeaefddc2a44a70a88ae47b6c62cd8b79
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-09-04 17:44:56 -0300

    Account viewModel and text validation

---
+++ Appetizers/Appetizers/Utilities/Extensions/String+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  String+Ext.swift
+//  Appetizers
+//
+//  Created by Pedro Boga on 04/09/22.
+//
+
+import Foundation
+
+extension String {
+    
+    var isValidEmail: Bool {
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: self)
+    }
+}
