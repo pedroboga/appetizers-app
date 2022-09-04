03f0cd5a6b1bb04c94da6e244015d1974441eab8
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-09-04 17:16:36 -0300

    Created the account view

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefils = false
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("E-mail", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        //
+                    } label: {
+                        Text("Save changes")
+                    }
+
+                } header: {
+                    Text("Personal Info")
+                }
+                Section {
+                    Group {
+                        Toggle("Extra Napkins", isOn: $extraNapkins)
+                        Toggle("Frequent Refils", isOn: $frequentRefils)
+                    }
+                    .tint(Color("brandPrimary"))
+                } header: {
+                    Text("Request")
+                }
+            }
+            .navigationTitle("Account")
         }
     }
 }
