12e693195284d75c3fb844b9037cd81b7f528243
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-09-05 15:46:51 -0300

    Added @FocusState to the form in order view

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -9,14 +9,34 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
+    
+    enum FormTextField {
+        case firstName, lastName, email
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit {
+                            focusedTextField = .lastName
+                        }
+                        .submitLabel(.next)
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit {
+                            focusedTextField = .email
+                        }
+                        .submitLabel(.next)
                     TextField("E-mail", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit {
+                            focusedTextField = nil
+                        }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                     DatePicker("Birthday", selection: $viewModel.user.birthdate, displayedComponents: .date)
@@ -41,6 +61,13 @@ struct AccountView: View {
                 }
             }
             .navigationTitle("Account")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Button("Dismiss") {
+                        focusedTextField = nil
+                    }
+                }
+            }
         }
         .onAppear {
             viewModel.retrieveUser()
